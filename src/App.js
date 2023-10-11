bf488fe4394f9622faea45ca3bf8d4da7b81c98c
Viktorija_Bosilkovska <victorija.bosilkovska@gmail.com>
2023-10-11 21:10:15 +0200

    add text area and delete button - html, css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Note from "./components/Note"
+import notes from "./notes"
+import CreateArea from "./components/CreateArea"
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
-      <Footer /> 
+      <CreateArea />
+      <div className="note-container">
+        {notes.map((note) => (
+          <Note key={note.id} title={note.title} content={note.content} />
+        ))}
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/notes.js
@@ -0,0 +1,54 @@
+const notes = [
+  {
+    id: 1,
+    title: 'Take out the trash',
+    content: 'Remember to take out the trash before 6 PM.'
+  },
+  {
+    id: 2,
+    title: 'Buy milk',
+    content: 'Don\'t forget to buy a gallon of milk from the store.'
+  },
+  {
+    id: 3,
+    title: 'Finish online course',
+    content: 'Complete the last module of the online course by tonight.'
+  },
+  {
+    id: 4,
+    title: 'Call Mom',
+    content: 'Give Mom a call to catch up and see how she\'s doing.'
+  },
+  {
+    id: 5,
+    title: 'Schedule dentist appointment',
+    content: 'Call the dentist office to schedule a check-up appointment.'
+  },
+  {
+    id: 6,
+    title: 'Prepare presentation',
+    content: 'Gather all necessary materials and rehearse for tomorrow\'s presentation.'
+  },
+  {
+    id: 7,
+    title: 'Pay utility bills',
+    content: 'Pay the electricity and water bills by the end of the week.'
+  },
+  {
+    id: 8,
+    title: 'Organize closet',
+    content: 'Declutter and organize the closet this weekend.'
+  },
+  {
+    id: 9,
+    title: 'Read chapter 5 of the book',
+    content: 'Read and take notes on chapter 5 for book club discussion.'
+  },
+  {
+    id: 10,
+    title: 'Plan weekend hiking trip',
+    content: 'Research and plan a hiking trip for the upcoming weekend.'
+  }
+];
+
+export default notes; 
